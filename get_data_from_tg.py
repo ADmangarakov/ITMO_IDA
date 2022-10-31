5128a26eee4b5360a5d95ce3325027293ea5c1ce
Mangarakov Alexandr <ad.mangarakov@cdek.ru>
2022-10-31 05:51:34 +0300

    test_2

---
+++ get_data_from_tg.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+
+with DAG(
+        dag_id='get_from_tg',
+        schedule_interval='@once',
+        start_date=datetime(2022, 10, 25),
+        catchup=False,
+        tags=['test']
+) as dag:
+    get_and_save = BashOperator(
+        task_id='get_from_tg_and_save',
+        bash_command='echo "get from tg success"',
+    )
+
+
+
+if __name__ == "__main__":
+    dag.cli()
